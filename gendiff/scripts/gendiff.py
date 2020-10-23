c940beb8f0a0b2b5241f01ea7e3b78a02639cca7
Ganeshahere <jamper1488@gmail.com>
2020-10-23 08:45:17 +0300

    added code coverage

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,13 +9,17 @@ from gendiff.generator import generate_diff
 
 
 def main():
-    """Print diff between two files."""
-    parser = argparse.ArgumentParser(description='Generate diff')
+    """Print diff between two files also argument parser."""
+    parser = argparse.ArgumentParser(
+            prog='gendiff',
+            usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
+            description='Generate diff')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
     parser.add_argument(
             '-f',
             '--format',
+            action='store',
             help='set format of output',
             )
     args = parser.parse_args()
