5761cccdde4a990967fc0f666ee83be39cd11358
Ganeshahere <jamper1488@gmail.com>
2020-10-31 13:22:18 +0300

    added compairing with dictionaries, also functions now in single file

--- gendiff/comparator.py
+++ gendiff/comparator.py
@@ -1,28 +1,81 @@
 # -*- coding: utf-8 -*-
 
 
-"""Comparator of files."""
+"""Functions to build difference."""
 
 import json
 
 
-def diff(file1, file2):
-    """Should return difference between two files."""
-    diff = {}
+def diff(dict1, dict2):
+    """Difference between first and second dicts."""
+    first_keys = dict1.keys()
+    second_keys = dict2.keys()
 
-    for key in file1:
-        if key in file2:
-            if file1[key] == file2[key]:
-                diff['  {key}'.format(key=key)] = file1[key]
-            else:
-                diff['- {key}'.format(key=key)] = file1[key]
-                diff['+ {key}'.format(key=key)] = file2[key]
-        else:
-            diff['- {key}'.format(key=key)] = file1[key]
+    add_keys = second_keys - first_keys
+    remove_keys = first_keys - second_keys
+    common_keys = first_keys & second_keys
 
-    for key in file2:
-        if key not in file1:
-            diff['+ {key}'.format(key=key)] = file2[key]
+    added = {key: dict2[key] for key in add_keys},
+    removed = {key: dict1[key] for key in remove_keys},
+    changed = {
+        key: {'old': dict1[key], 'new': dict2[key]}
+        for key in common_keys
+        if dict1[key] != dict2[key]
+    }
+    unchanged = {
+        key: dict1[key] for key in common_keys if dict1[key] == dict2[key]
+    }
+    return {
+        'added': added,
+        'removed': removed,
+        'changed': changed,
+        'unchanged': unchanged
+    }
 
-    print(''.json(json.dumps(diff, indent=2).split('"')))
-    return diff
+
+def build_message(diff):
+    message_format = ' {symbol} {key}: {value}'
+
+    unchanged_messages = [
+        message_format.format(symbol=' ', key=key, value=value)
+        for key, value in diff['unchanged'].items()
+    ]
+    changed_messages = sum(
+        [
+            [
+                message_format.format(symbol='+', key=key, value=value['new']),
+                message_format.format(symbol='-', key=key, value=value['old']),
+            ]
+            for key, value in diff['changed'].items()
+        ],
+        [],
+    )
+    removed_messages = [
+        message_format.format(symbol='-', key=key, value=value)
+        for key, value in diff['removed'].items()
+    ]
+    added_messages = [
+        message_format.format(symbol='+', key=key, value=value)
+        for key, value in diff['added'].items()
+    ]
+
+    message = [
+        '{',
+        *unchanged_messages,
+        *changed_messages,
+        *removed_messages,
+        *added_messages,
+        '}'
+    ]
+    return '\n'.join(message)
+
+
+def generate_diff(path_to_file1: str, path_to_file2: str):
+    """Generate message of difference between fwo files."""
+    with open(path_to_file1) as first_file:
+        first_data = json.load(first_file)
+    with open(path_to_file2) as second_file:
+        second_data = json.load(second_file)
+
+    difference = diff(first_data, second_data)
+    return build_message(difference)
--- tests/test_json_diff.py
+++ tests/test_json_diff.py
@@ -2,31 +2,33 @@
 
 """JSON files diff test."""
 
-import unittest
-import json
+import pytest
 
-from gendiff.comparator import diff
+from gendiff.comparator import diff, generate_diff
 
 
-class TestJson(unittest.TestCase):
-    """Test the JSON diff."""
+def test_dict_diff():
+    first = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22'}
+    second = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
+    difference = diff(first, second)
 
-    def test_file1_file2_diff(self):
-        data1 = {
-                'timeout': '20',
-                'verbose': 'true',
-                'host': 'hexlet.io',
-                }
-        data2 = {
-                'host': 'hexlet.io',
-                'timeout': '50',
-                'proxy': '123.234.53.22',
-                }
-        result = diff(data1, data2)
-        expect = {
-                '  host': 'hexlet.io',
-                '- timeout': '50',
-                '+ timeout': '20',
-                '+ verbose': 'true'
-                }
-        self.assertEqual(sorted(json.dumps(result)), sorted(json.dumps(expect)))
+    assert difference == {
+        'added': {'verbose': True},
+        'removed': {'proxy': '123.234.53.22'},
+        'changed': {'timeout': {'new': 20, 'old': 50}},
+        'unchanged': {'host': 'hexlet.io'}
+    }
+
+
+def test_message_from_diff(expected_message):
+    difference = generate_diff(
+        'tests/fixtures/before.json',
+        'tests/fixtures/after.json'
+    )
+    assert difference.split('\n') == expected_message
+
+
+@pytest.fixture
+def expected_message(request):
+    with open('tests/fixtures/expected_message.txt') as file:
+        yield file.read().splitlines()
