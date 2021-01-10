7a4d9f78c979fd096619c1d2d9fa2298170487ad
Ganeshahere <jamper1488@gmail.com>
2021-01-10 14:05:52 +0300

    parametrizes tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -7,65 +7,36 @@ import json
 from gendiff.comparator import generate_diff
 
 # showing which files used for testcases
-DATA_SET_FOR_TESTCASES = {
-    'complex_json': 'complex_json_files',
-    'format_json': 'complex_json_files',
-    'format_plain': 'complex_json_files'
-}
 
-# paths for input files
-DATA_SET_PATHS = {
-    'complex_json_files': (
-        'tests/fixtures/before.json',
-        'tests/fixtures/after.json',
+
+path_to_fixtures = 'tests/fixtures/'
+
+
+cases = [
+    (
+        path_to_fixtures + 'before.json',
+        path_to_fixtures + 'after.json',
+        'stylish',
+        path_to_fixtures + 'expected_stylish.txt'
     ),
-    'plain_yaml_files': (
-        'tests/fixtures/before.yaml',
-        'tests/fixtures/after.yaml'),
-}
-
-# paths for expected files for each testcase
-EXPECTED_RESULTS_PATHS = {
-    'complex_json': 'tests/fixtures/expected_stylish.txt',
-    'format_plain': 'tests/fixtures/expected_plain.txt',
-    'format_json': 'tests/fixtures/expected_json.json'
-}
-
-
-@pytest.fixture()
-def expected_results():
-    def get_expected_result(testcase):
-        filepath = EXPECTED_RESULTS_PATHS[testcase]
-        with open(filepath) as file:
-            return file.read()
-
-    return get_expected_result
+    (
+        path_to_fixtures + 'before.yaml',
+        path_to_fixtures + 'after.yaml',
+        'plain',
+        path_to_fixtures + 'expected_plain.txt'
+    ),
+    (
+        path_to_fixtures + 'before.json',
+        path_to_fixtures + 'after.json',
+        'json',
+        path_to_fixtures + 'expected_json.json'
+    )]
 
 
-@pytest.fixture()
-def data_sets():
-    def get_filepaths(testcase):
-        data_set = DATA_SET_FOR_TESTCASES[testcase]
-        return DATA_SET_PATHS[data_set]
-    return get_filepaths
-
-
-def test_complex_json(data_sets, expected_results):
-    testcase = 'complex_json'
-    format = 'stylish'
-    first, second = data_sets(testcase)
-    assert expected_results(testcase) == generate_diff(first, second, format)
-
-
-def test_format_plain(data_sets, expected_results):
-    testcase = 'format_plain'
-    format = 'plain'
-    first, second = data_sets(testcase)
-    assert expected_results(testcase) == generate_diff(first, second, format)
-
-
-def test_format_json(data_sets, expected_results):
-    testcase = 'format_json'
-    format = 'json'
-    first, second = data_sets(testcase)
-    assert json.loads(expected_results(testcase)) == json.loads(generate_diff(first, second, format))
+@pytest.mark.parametrize('path_to_first, path_to_second, form, expected', cases)
+def test_generate_diff(path_to_first, path_to_second, form, expected):
+    with open(expected, 'r') as file:
+        if form == 'json':
+            assert json.load(file) == json.loads(generate_diff(path_to_first, path_to_second, form))
+        else:
+            assert file.read() == generate_diff(path_to_first, path_to_second, form)
