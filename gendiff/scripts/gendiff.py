b349c8499868073a8e0e9998c27b8fa997503820
Ganeshahere <jamper1488@gmail.com>
2021-01-14 20:03:56 +0300

    naming qoute

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,8 +5,7 @@ from gendiff.comparator import generate_diff
 def main():
     """Run cli."""
     args = get_parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file, args.format)
-    print(diff)
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
