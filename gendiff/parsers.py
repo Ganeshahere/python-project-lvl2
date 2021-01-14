b2345d9fcd48204fc02c6989ca2dac8dc1993b0f
Ganeshahere <jamper1488@gmail.com>
2021-01-14 20:19:12 +0300

    fixed parsers

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -6,6 +6,7 @@ import os
 import yaml
 import argparse
 
+
 def get_extension(file_path):
     return os.path.splitext(file_path)[-1][1:]
 
@@ -23,6 +24,7 @@ def get_parsed_data(filepath):
     extension = get_extension(filepath)
     raw_data = read_data_from_file(filepath, extension)
     return raw_data
+
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Generate diff')
